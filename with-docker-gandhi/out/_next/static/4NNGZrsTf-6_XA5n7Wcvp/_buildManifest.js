00fcf3af6a286b9ab3cbfa728150437aca678fd2
bckim <bckim@wizzpay.co.kr>
2022-10-27 16:17:58 +0900

    export test

---
+++ with-docker-gandhi/out/_next/static/4NNGZrsTf-6_XA5n7Wcvp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/10904d47443138de.css","static/chunks/pages/index-185d44e6cb5a6462.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
